f308ca4a7f16c9372a92cd3b6a3c67458e56f0b3
osogrizz <lhtor@gmail.com>
2019-05-07 22:35:56 -0700

    init commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Whisper Sisters`,
+    description: `A cocktail bar`,
+    author: `Leo Torres @leo-torres.tech`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `whisper-sisters`,
+        short_name: `WHisper Sisters`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
