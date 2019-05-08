d0363a3d6ec8cee2e25b18310f0e127538165374
osogrizz <lhtor@gmail.com>
2019-05-08 09:35:41 -0700

    initial header component.

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,129 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import styled from 'styled-components'
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+const HeaderWrapper = styled.div`
+  margin: 0  auto;
+  max-width: 960px;
+  margin-bottom: 1.45rem;
+  background: transparent;
+  letter-spacing: 3px;
+  font-family: 'Raleway', sans-serif;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+`
+
+const NavMenu = styled.nav`
+  font-size: 1.2rem;
+  font-weight: 400;
+  letter-spacing: 3px;
+  margin: 0;
+  white-space: nowrap;
+ul {
+  display: flex;
+  justify-content: space-around;
+  list-style: none;
+  li {
+      border-top: 4px solid transparent;
+      transition: hover 0.15s ease-in-out;
+    &:hover {
+      border-top: 4px solid black;
+      transition: 0.15s ease-in-out;
+      a {
+        color: black;
+      }
+    }
+    div {
+      padding-top: 35px;
+    }
+  }
+}
+a {
+  padding-top: 35px;
+  text-transform: uppercase;
+  text-decoration: none;
+  color: #999;
+  transition: hover 0.15s ease-in-out;
+  &:hover {
+    color: black;
+    transition: 0.15s ease-in-out;
+  }
+}
+
+@media (max-width: 800px) {
+  font-size: 1rem;
+  margin: 0 80px;
+  a {
+    padding-top: 37px;
+  }
+}
+
+@media (max-width: 750px) {
+  font-size: .8rem;
+  margin: 0 80px;
+  a {
+    padding-top: 40px;
+  }
+}
+
+@media (max-width: 600px) {
+  /* font-size: .8rem; */
+  margin: 0;
+}
+`
+
+const activeLinkStyle = {
+  borderTop: `4px solid black`,
+  color: `black`,
+}
+
+const Header = () => (
+  <HeaderWrapper>
+    <NavMenu>
+      <ul>
+        <li>
+          <div>
+            <Link activeStyle={activeLinkStyle}
+              to="/">
+              Home
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div>
+            <Link activeStyle={activeLinkStyle}
+              to="/menu">
+              Menu
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div>
+            <Link activeStyle={activeLinkStyle}
+              to="/coffee">
+              Coffee
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div>
+            <Link activeStyle={activeLinkStyle}
+              to="/gallery">
+              Gallery
+            </Link>
+          </div>
+        </li>
+        <li>
+          <div>
+            <Link activeStyle={activeLinkStyle}
+              to="/contact">
+              Contact Us
+            </Link>
+          </div>
+        </li>
+      </ul>
+    </NavMenu>
+  </HeaderWrapper>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
